a56645731c858e9f1fec07fdc762addfc0e5f22c
Hercinger Viktor <hercinger.viktor@gmail.com>
2009-10-21 21:39:29 +0200

    Test statistics printer added

--- lib/main.c
+++ lib/main.c
@@ -27,6 +27,15 @@ typedef struct _MainTestContext
   LeakInfo        m_leak_suite;
 } MainTestContext;
 
+typedef enum
+{
+  STAT_VERB_NONE = 0,
+  STAT_VERB_SUMMARY,
+  STAT_VERB_SUITES,
+  STAT_VERB_FULL,
+  STAT_VERB_VERBOSE,
+} StatisticsVerbosity;
+
 static const TestContextFuncs g_main_test_context_funcs;
 static GOptionEntry g_main_opt_entries[];
 
@@ -35,6 +44,7 @@ static gboolean g_opt_fancy = FALSE;
 static gboolean g_opt_silent = FALSE;
 static gboolean g_opt_syslog = FALSE;
 static gint g_opt_priority = LOG_WARNING;
+static StatisticsVerbosity g_opt_stat_verb = STAT_VERB_SUMMARY;
 
 MainTestContext g_main_test_context;
 static gboolean g_main_test_context_init = FALSE;
@@ -102,6 +112,40 @@ _tinu_opt_priority(const gchar *opt G_GNUC_UNUSED, const gchar *value,
   return TRUE;
 }
 
+gboolean
+_tinu_opt_stat_verb(const gchar *opt G_GNUC_UNUSED, const gchar *value,
+  gpointer data, GError **error)
+{
+  if (!strcasecmp(value, "none"))
+    {
+      g_opt_stat_verb = STAT_VERB_NONE;
+    }
+  else if (!strcasecmp(value, "summary"))
+    {
+      g_opt_stat_verb = STAT_VERB_SUMMARY;
+    }
+  else if (!strcasecmp(value, "suites"))
+    {
+      g_opt_stat_verb = STAT_VERB_SUITES;
+    }
+  else if (!strcasecmp(value, "full"))
+    {
+      g_opt_stat_verb = STAT_VERB_FULL;
+    }
+  else if (!strcasecmp(value, "verbose"))
+    {
+      g_opt_stat_verb = STAT_VERB_VERBOSE;
+    }
+  else
+    {
+      g_set_error(error, log_error_main(), MAIN_ERROR_OPTIONS,
+                  "Unknown statistics verbosity `%s'", value);
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
 static GOptionEntry g_main_opt_entries[] = {
   { "fancy-log", 'c', 0, G_OPTION_ARG_NONE, (gpointer)&g_opt_fancy,
     "Colourfull logging to stderr", NULL },
@@ -115,6 +159,9 @@ static GOptionEntry g_main_opt_entries[] = {
   { "leak-watch", 'l', 0, G_OPTION_ARG_CALLBACK, (gpointer)&_tinu_opt_leakwatch,
     "Set leakwatch level (suite=Watch per suite; test=Watch per test; both=Both)",
     "watch-scope" },
+  { "results", 'R', 0, G_OPTION_ARG_CALLBACK, (gpointer)&_tinu_opt_stat_verb,
+    "Set statistics verbosity (none, summary (default), suites, full, verbose)",
+    "verbosity" },
   { NULL }
 };
 
@@ -168,6 +215,138 @@ _tinu_options(int *argc, char **argv[])
   return TRUE;
 }
 
+void
+_tinu_show_summary(TestStatistics *stat)
+{
+  if (g_opt_fancy)
+    {
+      if (stat->m_sigsegv)
+        {
+          fprintf(stderr, "Summary: "
+                          "\033[1;32mpassed: %d\033[0m; "
+                          "\033[1;31mfailed: %d\033[0m; "
+                          "\033[41m\033[1;37msegmentation faults: %d\033[0m\n",
+                  stat->m_passed, stat->m_failed, stat->m_sigsegv);
+        }
+      else
+        {
+          fprintf(stderr, "Summary: "
+                          "\033[1;32mpassed: %d\033[0m; "
+                          "\033[1;31mfailed: %d\033[0m\n",
+                  stat->m_passed, stat->m_failed);
+        }
+    }
+  else
+    fprintf(stderr, "Summary: passed: %d; failed: %d; segmentation faults: %d\n",
+            stat->m_passed, stat->m_failed, stat->m_sigsegv);
+}
+
+void
+_tinu_show_suite(TestSuite *suite)
+{
+  if (g_opt_fancy)
+    {
+      fprintf(stderr, "Suite   \033[35m%-*s\033[0m %s\n",
+              30, suite->m_name,
+              suite->m_passed ? "\033[32mpassed\033[0m" : "\033[31mfailed\033[0m");
+    }
+  else
+    {
+      fprintf(stderr, "Suite   %-*s %s\n",
+              30, suite->m_name,
+              suite->m_passed ? "passed" : "failed");
+    }
+}
+
+void
+_tinu_show_case(TestCase *test)
+{
+  int len;
+
+  if (g_opt_fancy)
+    {
+      len = fprintf(stderr, "   Case \033[35m%-*s\033[0m ", 33, test->m_name);
+
+      switch (test->m_result)
+        {
+          case TEST_PASSED :
+            fprintf(stderr, "\033[32mpassed\033[0m\n");
+            break;
+
+          case TEST_FAILED :
+            fprintf(stderr, "\033[31mfailed\033[0m\n");
+            break;
+
+          case TEST_SEGFAULT :
+            fprintf(stderr, "\033[41m\033[1;37msegfault\033[0m\n");
+            break;
+        }
+
+    }
+  else
+    {
+      fprintf(stderr, "   Case %-*s ", 33, test->m_name);
+
+      switch (test->m_result)
+        {
+          case TEST_PASSED :
+            fprintf(stderr, "passed\n");
+            break;
+
+          case TEST_FAILED :
+            fprintf(stderr, "failed\n");
+            break;
+
+          case TEST_SEGFAULT :
+            fprintf(stderr, "segfault\n");
+            break;
+        }
+    }
+}
+
+void
+_tinu_show_results()
+{
+  TestContext *self = (TestContext *)&g_main_test_context.m_super;
+  gint i, j;
+
+  TestSuite *suite;
+  TestCase *test;
+
+  if (g_opt_stat_verb == STAT_VERB_NONE)
+    return;
+
+  _tinu_show_summary(&self->m_statistics);
+
+  if (g_opt_stat_verb == STAT_VERB_SUMMARY)
+    return;
+
+  for (i = 0; i < self->m_suites->len; i++)
+    {
+      suite = g_ptr_array_index(self->m_suites, i);
+
+      _tinu_show_suite(suite);
+
+      if (g_opt_stat_verb > STAT_VERB_SUITES)
+        {
+          for (j = 0; j < suite->m_tests->len; j++)
+            _tinu_show_case(g_ptr_array_index(suite->m_tests, j));
+          fprintf(stderr, "\n");
+        }
+    }
+
+  if (g_opt_stat_verb <= STAT_VERB_FULL)
+    return;
+
+  fprintf(stderr, "Messages received:\n");
+  for (i = LOG_CRIT; i <= LOG_DEBUG; i++)
+    {
+      fprintf(stderr, "    %-*s %*d\n",
+              25, msg_format_priority(i),
+              10, self->m_statistics.m_messages[i]);
+    }
+}
+
 int
 tinu_main(int *argc, char **argv[])
 {
@@ -194,6 +373,8 @@ tinu_main(int *argc, char **argv[])
     }
 
   res = tinu_test_all_run((TestContext *)&g_main_test_context) ? 0 : 1;
+
+  _tinu_show_results();
 
   test_context_destroy((TestContext *)&g_main_test_context);
   g_free(basename);
