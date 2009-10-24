2f42c5a2a8e3d5573e40ef7572829d301e348f67
Hercinger Viktor <hercinger.viktor@gmail.com>
2009-10-24 23:03:20 +0200

    Leakwatcher off by default

--- lib/main.c
+++ lib/main.c
@@ -30,7 +30,7 @@ static gboolean g_opt_fancy = FALSE;
 static gboolean g_opt_silent = FALSE;
 static gboolean g_opt_syslog = FALSE;
 static gboolean g_opt_sighandle = TRUE;
-static gboolean g_opt_leakwatch = TRUE;
+static gboolean g_opt_leakwatch = FALSE;
 static gint g_opt_priority = LOG_WARNING;
 static StatisticsVerbosity g_opt_stat_verb = STAT_VERB_SUMMARY;
 
@@ -119,8 +119,8 @@ static GOptionEntry g_main_opt_entries[] = {
   { "results", 'R', 0, G_OPTION_ARG_CALLBACK, (gpointer)&_tinu_opt_stat_verb,
     "Set statistics verbosity (none, summary (default), suites, full, verbose)",
     "verbosity" },
-  { "no-leakwatch", 0, G_OPTION_FLAG_REVERSE, G_OPTION_ARG_NONE, (gpointer)&g_opt_leakwatch,
-    "Disable leakwatcher", NULL },
+  { "leakwatch", 0, 0, G_OPTION_ARG_NONE, (gpointer)&g_opt_leakwatch,
+    "Enable leakwatcher (warning: slows tests down by a significant ammount of time)", NULL },
   { "no-sighandle", 0, G_OPTION_FLAG_HIDDEN | G_OPTION_FLAG_REVERSE, G_OPTION_ARG_NONE, 
     (gpointer)&g_opt_sighandle,
     "Don't handle signals from test", NULL },
@@ -327,6 +327,7 @@ tinu_main(int *argc, char **argv[])
   g_main_test_context.m_sighandle = g_opt_sighandle;
   g_main_test_context.m_leakwatch = g_opt_leakwatch;
 
+  log_debug("Running tests", NULL);
   if (g_opt_suite)
     res = tinu_test_suite_run(&g_main_test_context, g_opt_suite);
   else
